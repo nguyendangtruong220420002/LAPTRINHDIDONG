79748166a3ac654acb77bf5f9eca225942f40557
nguyendangtruong220420002 <121849370+nguyendangtruong220420002@users.noreply.github.com>
2023-10-06 07:56:23 +0700

    oki

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Trang1 from './Trang/Trang1';
+import Trang2 from './Trang/Trang2';
+import Trang3 from './Trang/Trang3';
+import Trang4 from './Trang/Trang4';
+import Trang5 from './Trang/Trang5';
+import Trang6 from './Trang/Trang6';
+import React from 'react';
+export default function App() {
+  return (
+    //(<Trang1></Trang1>)
+    //(<Trang2> </Trang2>)
+    //(<Trang3></Trang3>)
+    //(<Trang4></Trang4>)
+    //(<Trang5></Trang5>)
+    (<Trang6></Trang6>)
+  );
+}
+
